e33a4932286771d21a351c3e0a6d50348df1147a
bruce5283 <bcarrington7@gmail.com>
2022-03-16 15:13:21 -0400

    Update function.js
    Updated rest time to work time

--- function.js
+++ function.js
@@ -66,7 +66,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s rest time";
+  document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s work time";
 
   // If the count down is finished, write some text
   if (distance < 0) {
